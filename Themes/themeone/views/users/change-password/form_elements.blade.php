aaa0e1d7ae299602eb26779aa7f43cf4405911e9
sasi@4858 <Sasidhar@conquerorstech.net>
2025-08-01 11:19:46 +0530

    up[date eye icons for visibility

--- Themes/themeone/views/users/change-password/form_elements.blade.php
+++ Themes/themeone/views/users/change-password/form_elements.blade.php
@@ -8,17 +8,20 @@
 
 						<span class="text-red">*</span>
 
-						{{ Form::password('old_password', $attributes = array('class'=>'form-control', 'placeholder' => getphrase('old_password'),
+	{{ Form::password('old_password', $attributes = array(
+		'class'=>'form-control pr-5', // Add right padding for the icon
+		'placeholder' => getphrase('old_password'),
+		'ng-model'=>'old_password',
+		'required'=> 'true',
+		'ng-class'=>'{"has-error": changePassword.old_password.$touched && changePassword.old_password.$invalid}',
+		'ng-minlength' => 5,
+		'id' => 'old_password' // Add ID to target it in JS
+	)) }}
+	<span onclick="togglePassword('old_password', this)" style="position:absolute; top: 50%; right: 10px; transform: translateY(-50%); cursor:pointer;">
+		<i class="fa fa-eye"></i>
+	</span>
 
-							'ng-model'=>'old_password',
 
-							'required'=> 'true', 
-
-							'ng-class'=>'{"has-error": changePassword.old_password.$touched && changePassword.old_password.$invalid}',
-
-							'ng-minlength' => 5
-
-						)) }}
 
 	<div class="validation-error" ng-messages="changePassword.old_password.$error" >
 
@@ -40,17 +43,18 @@
 
 						<span class="text-red">*</span>
 
-						{{ Form::password('password', $attributes = array('class'=>'form-control', 'placeholder' => getphrase('new_password'),
-
-						'ng-model'=>'password',
-
-							'required'=> 'true', 
-
-							'ng-class'=>'{"has-error": changePassword.password.$touched && changePassword.password.$invalid}',
-
-							'ng-minlength' => 5
-
-						)) }}
+						{{ Form::password('password', $attributes = array(
+		'class'=>'form-control pr-5', // padding-right for eye icon
+		'placeholder' => getphrase('new_password'),
+		'ng-model'=>'password',
+		'required'=> 'true',
+		'ng-class'=>'{"has-error": changePassword.password.$touched && changePassword.password.$invalid}',
+		'ng-minlength' => 5,
+		'id' => 'password' // required for JS toggle
+	)) }}
+	<span onclick="togglePassword('password', this)" style="position:absolute; top: 50%; right: 10px; transform: translateY(-50%); cursor:pointer;">
+		<i class="fa fa-eye"></i>
+	</span>
 
 	<div class="validation-error" ng-messages="changePassword.password.$error" >
 
@@ -70,19 +74,19 @@
 
 						<span class="text-red">*</span>
 
-						{{ Form::password('password_confirmation', $attributes = array('class'=>'form-control', 'placeholder' => getphrase('retype_password'),
-
-						'ng-model'=>'password_confirmation',
-
-							'required'=> 'true', 
-
-							'ng-class'=>'{"has-error": changePassword.password_confirmation.$touched && changePassword.password_confirmation.$invalid}',
-
-							'compare-to' =>"password",
-
-							'ng-minlength' => 5
-
-						)) }}
+						{{ Form::password('password_confirmation', $attributes = array(
+		'class'=>'form-control pr-5',
+		'placeholder' => getphrase('retype_password'),
+		'ng-model'=>'password_confirmation',
+		'required'=> 'true',
+		'ng-class'=>'{"has-error": changePassword.password_confirmation.$touched && changePassword.password_confirmation.$invalid}',
+		'compare-to' =>"password",
+		'ng-minlength' => 5,
+		'id' => 'password_confirmation'
+	)) }}
+	<span onclick="togglePassword('password_confirmation', this)" style="position:absolute; top: 50%; right: 10px; transform: translateY(-50%); cursor:pointer;">
+		<i class="fa fa-eye"></i>
+	</span>
 
 	<div class="validation-error" ng-messages="changePassword.password_confirmation.$error" >
 
@@ -107,3 +111,18 @@
 							ng-disabled='!changePassword.$valid' >{{ $button_name }}</button>
 
 						</div>
+                        <script>
+	function togglePassword(fieldId, iconElement) {
+		const input = document.getElementById(fieldId);
+		const icon = iconElement.querySelector('i');
+		if (input.type === "password") {
+			input.type = "text";
+			icon.classList.remove('fa-eye');
+			icon.classList.add('fa-eye-slash');
+		} else {
+			input.type = "password";
+			icon.classList.remove('fa-eye-slash');
+			icon.classList.add('fa-eye');
+		}
+	}
+</script>
